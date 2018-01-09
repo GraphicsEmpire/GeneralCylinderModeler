7f425fdeea56fda5ddf325700301d7e0df34e9c5
Pourya Shirazian <Pourya.Shirazian@intusurg.com>
2018-01-08 22:37:22 -0800

    working on crom curve interface

---
+++ src/catmullromcurve.cpp
@@ -0,0 +1,1 @@
+#include "catmullromcurve.h"
--- src/catmullromcurve.h
+++ src/catmullromcurve.h
@@ -5,14 +5,21 @@
 #include "linalg/vec.h"
 
 using namespace std;
-using namespace nb;
+using namespace nb::linalg;
 
 class CatmullRomCurve {
 public:
   CatmullRomCurve();
   CatmullRomCurve(const CatmullRomCurve& rhs);
 
-  void AddCtrlPoint(const nb::vec3& p);
+  void AddCtrlPoint(const nb::linalg::Vec3<float>& p);  
+  U32 CountCtrlPoints() const;
+  void ComputeCurvePoints();
+
+  Vec3<float> GetPosition(float t);
+  Vec3<float> GetTangent(float t);
+  Vec3<float> GetAcceleration(float t);
+
 protected:
   vector<float> mRegularPoints;
   vector<float> mCtrlPoints;
